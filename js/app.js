6535cc0b5943d1c54caa1ec4327341ff321d5168
Justin Dela Cruz <justindelacruz@server.fake>
2014-09-24 23:17:40 -0700

    Make the hero dynamically sized

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+nyan = function() {
+    var resizeHero = function() {
+        var w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+        console.log(height);
+        $('.hero').css('max-height', height);
+        $('.head').css('max-height', height);
+    };
+
+    var setupEvents = function() {
+        window.addEventListener('resize', resizeHero);
+    }
+
+    var init = function() {
+        resizeHero();
+        setupEvents();
+    };
+
+    return{
+        init:init
+    }
+}();
+
+nyan.init();
