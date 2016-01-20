2bcd6983e84495c19ba4b4b957ddfade491c4702
Justin Dela Cruz <justindelacruz@server.fake>
2016-01-19 16:30:14 -0800

    Add browserify. Move compiled files into /static directory.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "static/css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "js"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
